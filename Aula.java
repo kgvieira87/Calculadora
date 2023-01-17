ec623494b53512fd521fca12296169326b2773b1
kgvieira87 <120027507+kgvieira87@users.noreply.github.com>
2023-01-16 23:58:11 -0300

    Add files via upload

---
+++ Aula.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class Aula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner sc = new Scanner (System.in);
+	
+	
+	
+
+int produto;
+int x = 1 ;
+
+System.out.println("Informe o múltiplo da tabuada:");	
+produto = sc.nextInt();
+
+for (produto = 1 ; x<=10 ; ++x)
+
+
+for (x=1; x<=10 ; ++x ){
+	
+	System.out.println(x + " x "+ produto + " = " + x*produto);	
+}
+		
+		
+	}
+
+}
